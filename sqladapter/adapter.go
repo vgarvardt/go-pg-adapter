51e0320defedfa60d691f5d560285046475ee08d
Vladimir Garvardt <vgarvardt@gmail.com>
2019-07-16 18:10:40 +0200

    Added adapter for sql.Conn

--- sqladapter/adapter.go
+++ sqladapter/adapter.go
@@ -1,36 +1,65 @@
 package sqladapter
 
 import (
+	"context"
 	"database/sql"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/vgarvardt/go-pg-adapter"
 )
 
-// Adapter is the adapter type for sqlx.DB connection type
-type Adapter struct {
-	conn *sqlx.DB
+// DB is the adapter type for sqlx.DB connection type
+type DB struct {
+	db *sqlx.DB
 }
 
 // New instantiates sqlx.DB connection adapter from sql.DB connection
-func New(conn *sql.DB) *Adapter {
+func New(db *sql.DB) *DB {
 	// The driverName of the original database is required for named query support - we do not use it here
-	return &Adapter{sqlx.NewDb(conn, "")}
+	return &DB{sqlx.NewDb(db, "")}
 }
 
 // NewX instantiates sqlx.DB connection adapter
-func NewX(conn *sqlx.DB) *Adapter {
-	return &Adapter{conn}
+func NewX(db *sqlx.DB) *DB {
+	return &DB{db}
+}
+
+// Conn is the adapter type for sql.Conn connection type
+type Conn struct {
+	conn *sql.Conn
+}
+
+func NewConn(conn *sql.Conn) *Conn {
+	return &Conn{conn}
 }
 
 // Exec runs a query and returns an error if any
-func (a *Adapter) Exec(query string, args ...interface{}) error {
-	_, err := a.conn.Exec(query, args...)
+func (a *DB) Exec(query string, args ...interface{}) error {
+	_, err := a.db.Exec(query, args...)
 	return err
 }
 
 // SelectOne runs a select query and scans the object into a struct or returns an error
-func (a *Adapter) SelectOne(dst interface{}, query string, args ...interface{}) error {
-	if err := a.conn.Get(dst, query, args...); err != nil {
+func (a *DB) SelectOne(dst interface{}, query string, args ...interface{}) error {
+	if err := a.db.Get(dst, query, args...); err != nil {
+		if err == sql.ErrNoRows {
+			return pgadapter.ErrNoRows
+		}
+		return err
+	}
+
+	return nil
+}
+
+// Exec runs a query and returns an error if any
+func (a *Conn) Exec(query string, args ...interface{}) error {
+	_, err := a.conn.ExecContext(context.Background(), query, args...)
+	return err
+}
+
+// SelectOne runs a select query and scans the object into a struct or returns an error
+func (a *Conn) SelectOne(dst interface{}, query string, args ...interface{}) error {
+	if err := a.conn.QueryRowContext(context.Background(), query, args...).Scan(dst); err != nil {
 		if err == sql.ErrNoRows {
 			return pgadapter.ErrNoRows
 		}
